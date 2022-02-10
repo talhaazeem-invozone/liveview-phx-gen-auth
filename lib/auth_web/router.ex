95ca10a50390d5aa80fab8e4343f8c8a3603e7aa
talhaazeem-invozone <talha.azeem@invozone.com>
2022-02-10 12:10:29 +0500

    added the dependency of phx_gen_auth

--- lib/auth_web/router.ex
+++ lib/auth_web/router.ex
@@ -1,6 +1,8 @@
 defmodule AuthWeb.Router do
   use AuthWeb, :router
 
+  import AuthWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,16 +10,11 @@ defmodule AuthWeb.Router do
     plug :put_root_layout, {AuthWeb.LayoutView, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
 
   pipeline :api do
     plug :accepts, ["json"]
-  end
-
-  scope "/", AuthWeb do
-    pipe_through :browser
-
-    live "/", PageLive, :index
   end
 
   # Other scopes may use custom stacks.
@@ -39,5 +36,38 @@ defmodule AuthWeb.Router do
       pipe_through :browser
       live_dashboard "/dashboard", metrics: AuthWeb.Telemetry
     end
+  end
+
+  ## Authentication routes
+
+  scope "/", AuthWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    get "/users/register", UserRegistrationController, :new
+    post "/users/register", UserRegistrationController, :create
+    get "/users/log_in", UserSessionController, :new
+    post "/users/log_in", UserSessionController, :create
+    get "/users/reset_password", UserResetPasswordController, :new
+    post "/users/reset_password", UserResetPasswordController, :create
+    get "/users/reset_password/:token", UserResetPasswordController, :edit
+    put "/users/reset_password/:token", UserResetPasswordController, :update
+  end
+
+  scope "/", AuthWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live "/", PageLive, :index
+    get "/users/settings", UserSettingsController, :edit
+    put "/users/settings", UserSettingsController, :update
+    get "/users/settings/confirm_email/:token", UserSettingsController, :confirm_email
+  end
+
+  scope "/", AuthWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+    get "/users/confirm", UserConfirmationController, :new
+    post "/users/confirm", UserConfirmationController, :create
+    get "/users/confirm/:token", UserConfirmationController, :confirm
   end
 end
